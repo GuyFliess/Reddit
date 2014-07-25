bfe809cd1f6bdd3b36816bfd967a7a95dc09713a
srmish <srmish@gmail.com>
2014-07-25 18:26:44 +0300

    Added language resource files

---
+++ lang/en.js
@@ -0,0 +1,41 @@
+MENU_LOGIN_LOGOUT = 'Login/Logout';
+MENU_GOTO_SUBREDDIT = 'Goto Subreddit';
+MENU_SEARCH = 'Search';
+MENU_OPEN_IN_BROWSER = 'Open In Browser';
+
+LEGEND_SCROLL_ARTICLE = 'Scroll Article';
+LEGEND_SCROLL_PAGE = 'Scroll Page';
+LEGEND_VIEW_ARTICLE = 'View Article';
+LEGEND_VIEW_COMMENTS = 'View Comments';
+LEGEND_CLOSE_IMAGE = 'Close Image/Video';
+LEGEND_MENU = 'Menu';
+LEGEND_SUBREDDITS = 'Subreddits';
+LEGEND_MORE_KEYS = 'More Keys';
+LEGEND_UPVOTE = 'Upvote';
+LEGEND_DOWNVOTE = 'Downvote';
+LEGEND_SLIDESHOW_VIEW = 'Slideshow View';
+LEGEND_EDIT_SUBREDDIT = 'Edit Subreddit (When in Subreddits menu)';
+LEGEND_TOGGLE_LEGEND = 'Toggle legend';
+
+BUTTON_YES = "YES";
+BUTTON_NO = "NO";
+BUTTON_OK = "OK";
+
+INFO_LOGIN = 'logged in as: ';
+INFO_SUBMITTED_BY = 'submitted by ';
+INFO_COMMENTS = ' comments';
+
+PROMPT_LOGOUT_1 = 'Currently logged in as "' ;
+PROMPT_LOGOUT_2 = '". Would you like to logout?';
+PROMPT_LOGIN = 'Currently logged out. Would you like to login to reddit?';
+PROMPT_LOGIN_SUCCESS = 'Login succeeded';
+PROMPT_LOGIN_FAILED = 'Login failed';
+PROMPT_NEED_LOGIN = 'In order to upvote/downvote, you must be logged in';
+PROMPT_CANT_EDIT = "Can't edit the first subreddit entry (FRONTPAGE)";
+PROMPT_TIMEOUT = 'Request timed-out. Make sure you are still connected to the Internet';
+
+TITLE_SEARCH = "Search Reddit";
+TITLE_USERNAME = "Username";
+TITLE_PASSWORD = "Password";
+TITLE_SUBREDDIT = "Choose a subreddit";
+DESC_SUBREDDIT = "(for example \"science\")";
---
+++ lang/he.js
@@ -0,0 +1,41 @@
+MENU_LOGIN_LOGOUT = 'התחבר/התנתק';
+MENU_GOTO_SUBREDDIT = 'מעבר לסאברדיט';
+MENU_SEARCH = 'חיפוש';
+MENU_OPEN_IN_BROWSER = 'פתח בדפדפן';
+
+LEGEND_SCROLL_ARTICLE = 'דפדוף במאמרים';
+LEGEND_SCROLL_PAGE = 'דפדוף בדפים';
+LEGEND_VIEW_ARTICLE = 'הצגת מאמר';
+LEGEND_VIEW_COMMENTS = 'הצגת תגובות';
+LEGEND_CLOSE_IMAGE = 'סגירת תמונה/וידאו';
+LEGEND_MENU = 'תפריט';
+LEGEND_SUBREDDITS = 'סאברדיטים';
+LEGEND_MORE_KEYS = 'מקשים נוספים';
+LEGEND_UPVOTE = 'הצבע למעלה';
+LEGEND_DOWNVOTE = 'הצבע למטה';
+LEGEND_SLIDESHOW_VIEW = 'מבט שקופיות';
+LEGEND_EDIT_SUBREDDIT = 'ערוך סאברדיט (כאשר בתפריט סאברדיטים)';
+LEGEND_TOGGLE_LEGEND = 'Toggle legend';
+
+BUTTON_YES = "כן";
+BUTTON_NO = "לא";
+BUTTON_OK = "אשר";
+
+INFO_LOGIN = 'מחובר כ: ';
+INFO_SUBMITTED_BY = 'הועלה על ידי: ';
+INFO_COMMENTS = ' תגובות';
+
+PROMPT_LOGOUT_1 = 'כרגע מחובר כ "' ;
+PROMPT_LOGOUT_2 = '". האם תרצה להתנתק?';
+PROMPT_LOGIN = 'כרגע מנותק. האם תרצה להתחבר?';
+PROMPT_LOGIN_SUCCESS = 'ההתחברות הושלמה';
+PROMPT_LOGIN_FAILED = 'ההתחברות נכשלה';
+PROMPT_NEED_LOGIN = 'בשביל להצביע, עליך להיות מחובר';
+PROMPT_CANT_EDIT = "לא ניתן לערוך את הסאברדיט הראשון (FRONTPAGE)";
+PROMPT_TIMEOUT = 'הבקשה נכשלה. אנא בדוק שחיבור האינטרנט זמין';
+
+TITLE_SEARCH = "חפש ברדיט";
+TITLE_USERNAME = "שם משתמש";
+TITLE_PASSWORD = "סיסמא";
+TITLE_SUBREDDIT = "בחר סאברדיט";
+DESC_SUBREDDIT = "(לדוגמא \"science\")";
