45eae79779ef3d6c7a67e3e8ede0e1a5e9987b02
Guyfliess <guyfliess@gmail.com>
2014-09-20 17:06:35 +0300

    removed onLoad stuff

--- app/init.js
+++ app/init.js
@@ -11,27 +11,14 @@ function onDestroy () {
 
 }
 
-var WIDGET = new Common.API.Widget();        // For sendReadyEvent()
-alert("Define WIDEGT");
-//var TVKEY  = new Common.API.TVKeyValue();    // Remote controller key value object
 var PL_NNAVI_STATE_BANNER_VOL = 1;
-
-MainonLoad = function () {
-    WIDGET.sendReadyEvent();
-};
-
-window.onShow = function (e) {
+window.onShow = function () {
     var NNaviPlugin = caph.platform.dtv.Device.plugin('NNAVI');
     NNaviPlugin.SetBannerState(PL_NNAVI_STATE_BANNER_VOL);
 
     caph.platform.dtv.Device.unRegisterKey(caph.platform.Key.VOL_UP);
     caph.platform.dtv.Device.unRegisterKey(caph.platform.Key.VOL_DOWN);
     caph.platform.dtv.Device.unRegisterKey(caph.platform.Key.MUTE);
-    caph.platform.dtv.Device.unRegisterKey(caph.key.VOL_UP);
-    alert("unregistered keys vol_up");
-    /**
-     * code
-     */
 };
 
 alert("init.js loaded.");
